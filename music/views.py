68d34f18b45cfb566a179be9406e8364df7c4f99
Jason Mize <jasonemize@gmail.com>
2016-09-20 17:40:16 -0400

    admin working, basic web html displaying

--- music/views.py
+++ music/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 
+# from .models import Artist
 
 def index(request):
 
-    return render (request, music.index.html)
+    # music = music.objects.all()
 
+    # context = {
+    #     "music" = music
+    # }
+
+    return render (request, "music/index.html")
+
+
+# def Artists (request, id):
+#     artists = get_object_or_404(Student, pk=id)
+
+#     context = {
+#         "students":students
+#     }
+
+#     return render (request, "students/student.html", context)
 
 
 
